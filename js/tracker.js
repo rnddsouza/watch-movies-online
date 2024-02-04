1da877847e1b57e678a811c1f8a097c1bb1a33be
veigasjeff <154069224+rnddsouza@users.noreply.github.com>
2024-02-04 06:47:16 +0530

    update final

--- js/tracker.js
+++ js/tracker.js
@@ -3,7 +3,7 @@
 (function () {
     var script = document.createElement('script');
     script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-SZCKDE9NP9';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-FJQPRWE3C5';
   
     document.head.appendChild(script);
   
@@ -11,6 +11,6 @@
       window.dataLayer = window.dataLayer || [];
       function gtag() { dataLayer.push(arguments); }
       gtag('js', new Date());
-      gtag('config', 'G-SZCKDE9NP9');
+      gtag('config', 'G-FJQPRWE3C5');
     };
   })();
